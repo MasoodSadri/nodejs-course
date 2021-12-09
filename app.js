57dee0b46593151d308ef1f2cf9f9cac52d15e1c
Masood <masood@Masoods-MacBook-Pro.local>
2021-12-09 15:28:17 +0330

    adding html files

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h2>Not Found.</h2>')
+})
+
+app.listen(8000)
